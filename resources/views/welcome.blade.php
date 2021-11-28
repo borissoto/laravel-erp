b09a09690f7d2ab11d849ec7c92bd88568c011cb
Boris <borissoto@gmail.com>
2021-11-28 17:32:02 -0400

    testing paths

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,10 +14,10 @@
         <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
          <!-- Bootstrap CSS -->
          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-         <link rel="stylesheet" href="{{asset('/css/mindex.css')}}">
+         <link rel="stylesheet" href="/css/mindex.css">
          <!--datables estilo bootstrap 4 CSS-->  
          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-         <script src="{{asset('/js/maindex.js')}}"></script>
+         <script src="/js/maindex.js"></script>
          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
  
@@ -32,7 +32,7 @@
         </style>
 
           {{-- Tailwind --}}
-        <link rel="stylesheet" href="{{ asset('/css/tailwind.css') }}">
+        <link rel="stylesheet" href="/css/tailwind.css">
 
         <style>
             body {
@@ -326,8 +326,8 @@
 
         {{-- </div> --}}
         
-            <script src="{{asset('/js/particles.js')}}"></script>
-            <script src="{{asset('/js/particles-app.js')}}"></script>
+            <script src="/js/particles.js"></script>
+            <script src="/js/particles-app.js"></script>
         
         
         <!-- Global site tag (gtag.js) - Google Analytics -->
